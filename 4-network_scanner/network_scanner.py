af118ff2f79c01a84f7bb8677ab42823a1d17b32
slightlynervousdoggo.com <github@slightlynervousdoggo.com>
2020-08-03 11:53:52 -0400

    Completed "network-scanner" from section 4

---
+++ 4-network_scanner/network_scanner.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Target network to scan")
+    options = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify a target network, use --help for more info.")
+    return options
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
+
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
