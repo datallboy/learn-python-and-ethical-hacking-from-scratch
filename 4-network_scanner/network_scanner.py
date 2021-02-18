2968e8102cb89051a9b812a53d839dd313618c89
slightlynervousdoggo.com <github@slightlynervousdoggo.com>
2021-02-18 14:26:08 -0500

    Fix broadcast MAC address

--- 4-network_scanner/network_scanner.py
+++ 4-network_scanner/network_scanner.py
@@ -15,7 +15,7 @@ def get_arguments():
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff")
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
 
